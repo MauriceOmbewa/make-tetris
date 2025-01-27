5c0f9350fdd40640794d76f7271d578cfc0c1a6d
mombewa <ombewamaurice79@gmail.com>
2025-01-27 16:26:26 +0300

    feat: Add RandomTetromino function for random Tetromino selection

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const width = 10;
 let arr = Array.from({length: length}, () => new Array(width).fill(0))
 
 // console.log(arr);
+// RandomTetromino();
+
+function RandomTetromino(){
+    let num = 10;
+    while(num > 5){
+        num = Math.floor(Math.random()*10)
+    }
+    // console.log(num);
+    return num;
+}
