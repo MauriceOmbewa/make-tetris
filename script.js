572aeb0d70ed02ea79181ed338757b41d075ff41
mombewa <ombewamaurice79@gmail.com>
2025-01-28 14:43:19 +0300

    feat: add function (Movement) to push the tetromino horizontally either left or right

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ const tetrominoColor = [
 
 // console.log(arr);
 let num = RandomTetromino();
-console.log(num)
+// console.log(num)
 // // StartPosition(num);
 // console.log(tetromino[num].length)
-console.log(Color(num))
+// console.log(Color(num))
 
 function RandomTetromino(){
     let num = 10;
@@ -47,3 +47,86 @@ function StartPosition(num, arr, tetromino){
 function Color(num){
     return tetrominoColor[num];
 }
+
+// let arr5 = [
+//     [0, 0, 1, 1, 0],
+//     [0, 0, 1, 0, 0]
+// ]
+
+// Movement(arr5, 'right')
+
+function  Movement(arr, direction){
+    if (direction === 'left'){
+        for (let row = 0; row < arr.length; row++){
+            for (let index = 0; index < arr[row].length; index++){
+                if (arr[row][index] === 1){
+                    arr[row][index-1] = 1
+                    arr[row][index] = 0
+                }
+            }
+        }
+        console.log(arr)
+    } else if (direction === 'right'){
+        for (let row = 0; row < arr.length; row++){
+            for (let index = arr[row].length-1; index >= 0; index--){
+                if (arr[row][index] === 1){
+                    arr[row][index+1] = 1
+                    arr[row][index] = 0
+                }
+            }
+        }
+        console.log(arr)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let row = 0; row < arr.length; row++){
+//     for (let index = 0; index < arr[row].length; index++){
+//         if (arr[row][index] === 1 && line === 0){
+//             // arr[row][index-1] = 1
+//             lines[line] = row
+//             line++
+//             break
+//         } else if (arr[row][index] === 1 && line === 1){
+//             lines[line] = row
+//             line++
+//             break
+//         }
+//     }
+// }
